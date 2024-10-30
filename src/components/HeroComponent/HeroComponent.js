922d3580a86e9893e825073e54a24ee46dceff54
Tushar Sharma <tussharkumar.sh@gmail.com>
2024-10-30 16:13:54 +0530

    for loop added in hero component

--- src/components/HeroComponent/HeroComponent.js
+++ src/components/HeroComponent/HeroComponent.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import HeroCardComponent from './HeroCardComponent';
-import logoImage from '../../logo.svg';
 
 function HeroComponent() {
+
+    const products = [
+        { id: 1, productName: 'Product 1', productDescription: 'Product description', productImage: "https://picsum.photos/id/30/1280/901" },
+        { id: 2, productName: 'Product 2', productDescription: 'Product description', productImage: "https://picsum.photos/id/30/1280/901" },
+        { id: 3, productName: 'Product 3', productDescription: 'Product description', productImage: "https://picsum.photos/id/30/1280/901" },
+        { id: 4, productName: 'Product 4', productDescription: 'Product description', productImage: "https://picsum.photos/id/30/1280/901" },
+    ];
 
     return <>
 
         <div className="container my-5">
             <div className='row'>
-                <div className='col-md-3'>
-                    <HeroCardComponent productName="Product Name 1" productDescription="description text" productImage={logoImage} />
-                </div>
-
-                <div className='col-md-3'>
-                    <HeroCardComponent productName="Product Name 2" productDescription="description text" productImage={logoImage} />
-                </div>
-
-                <div className='col-md-3'>
-                    <HeroCardComponent productName="Product Name 3" productDescription="description text" productImage={logoImage} />
-                </div>
-
-                <div className='col-md-3'>
-                    <HeroCardComponent productName="Product Name 4" productDescription="description text" productImage={logoImage} />
-                </div>
+                {
+                    products.map(product => {
+                        return <div className='col-md-3'>
+                            <HeroCardComponent productName={product.productName} productDescription={product.productDescription} productImage={product.productImage} />
+                        </div>
+                    })
+                }
             </div>
 
         </div>
