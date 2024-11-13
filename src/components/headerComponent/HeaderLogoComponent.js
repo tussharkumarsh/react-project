4ac941177464a08735a29eeffc1fd3234a873c40
Tushar Sharma <tussharkumar.sh@gmail.com>
2024-11-13 22:24:01 +0530

    change in component

--- src/components/headerComponent/HeaderLogoComponent.js
+++ src/components/headerComponent/HeaderLogoComponent.js
@@ -2,8 +2,6 @@ import React from 'react'
 import logoImage from '../../logo.svg';
 
 function HeaderLogoComponent() {
-    return <>
-        <img src={logoImage} className='logo' />
-    </>
+    return <img src={logoImage} className='logo' />
 }
 export default HeaderLogoComponent;
