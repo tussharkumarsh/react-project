d40c3c00f8a1f3a23b28e02872348ae49e11c592
Ashish Sharma <92317433+ashishaugust22@users.noreply.github.com>
2024-11-15 19:48:52 +0100

    Home Page changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
-import HeaderComponent from './components/HeaderComponent/HeaderComponent'
-import HeroComponent from './Features/Component/HeroComponent/HeroComponent'
+import HeaderComponent from './components/headerComponent/HeaderComponent';
+import HomePage from "./Features/home-page/HomePage";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div>
       <HeaderComponent />
-      <HeroComponent />
+      <HomePage />
     </div>
   );
 }
