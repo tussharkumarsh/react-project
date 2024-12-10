d1956032f4227afa2a67f09d33396fa4a92ffd4c
Tushar Sharma <tussharkumar.sh@gmail.com>
2024-12-10 21:52:34 +0530

    product rating

--- src/Features/home-page/components/ProductRating.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function ProductRating(props){
-    return <span>Rating: <b>{props.rating.rate}</b></span>
-}
-export default ProductRating;
---
+++ src/Features/home-page/components/ProductRating.tsx
@@ -0,0 +1,4 @@
+function ProductRating(props) {
+    return <span>Rating: <b>{props.rating.rate}</b></span>;
+}
+export default ProductRating;
