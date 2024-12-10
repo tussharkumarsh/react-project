d8f7d67fbee80f6f4874a604580dc79d299a02fa
Tushar Sharma <tussharkumar.sh@gmail.com>
2024-12-10 21:23:03 +0530

    update to tsx product rating

--- src/Features/home-page/components/ProductRatingCount.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-function ProductRatingCount(props){
-    return <span>Count: <b>{props.rating.count}</b></span>
-}
-export default ProductRatingCount;
---
+++ src/Features/home-page/components/ProductRatingCount.tsx
@@ -0,0 +1,4 @@
+function ProductRatingCount(props) {
+    return <span>Count: <b>{props.rating.count}</b></span>;
+}
+export default ProductRatingCount;
