04b7305c70fcfdccc42742661d393cde813c9a9f
Tushar Sharma <tussharkumar.sh@gmail.com>
2024-12-11 19:07:52 +0530

    header tsx update

--- src/components/Header-component/HeaderComponent.tsx
+++ src/components/Header-component/HeaderComponent.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import './HeaderComponent.scss'
+import './HeaderComponent.scss';
 import HeaderLogoComponent from './HeaderLogoComponent';
 import HeaderLinksComponent from './HeaderLinksComponent';
 import HeaderButtonComponent from './HeaderButtonComponent';
 
 function HeaderComponent() {
   return (
-      <div className='topHeader'>
-        <HeaderLogoComponent/>
-        <HeaderLinksComponent/>
-        <HeaderButtonComponent/>
-      </div>
+    <div className='topHeader'>
+      <HeaderLogoComponent />
+      <HeaderLinksComponent />
+      <HeaderButtonComponent />
+    </div>
   );
 }
 export default HeaderComponent;
