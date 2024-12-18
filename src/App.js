a342cf3f01914140fb8f6cdac82bc339317f460c
Tushar Sharma <tussharkumar.sh@gmail.com>
2024-12-18 23:12:40 +0530

    added cart component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.scss';
 import HeaderComponent from './components/header-component/HeaderComponent';
 import HomePage from "./Features/home-page/HomePage";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <HeaderComponent />
-      <HomePage />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
