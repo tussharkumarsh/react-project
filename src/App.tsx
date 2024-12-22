ab6bd26c0a6e40e4cf9ee4e2115b643aae83eea2
Tushar Sharma <tussharkumar.sh@gmail.com>
2024-12-22 20:16:39 +0530

    product detail page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import HeaderComponent from './components/header-component/HeaderComponent';
 import HomePage from "./features/home-page/HomePage";
 import CartPage from "./features/cart-page/CartPage";
+import ProductDetail from 'features/home-page/container/productDetail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' Component={HomePage} />
           <Route path='/cart' Component={CartPage} />
+          <Route path='/product-detail' Component={ProductDetail} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/features/home-page/container/productDetail.tsx
@@ -0,0 +1,8 @@
+function ProductDetail() {
+    return (
+        <div>
+            <h1>Product Detail</h1>
+        </div>
+    );
+}
+export default ProductDetail;
