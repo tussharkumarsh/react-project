0b526e95fbbaf69825b55f3d5f4d868c3cf1cd74
Tushar Sharma <tussharkumar.sh@gmail.com>
2025-01-03 13:27:09 +0530

    css change header

--- src/components/header-component/HeaderComponent.tsx
+++ src/components/header-component/HeaderComponent.tsx
@@ -11,7 +11,7 @@ function HeaderComponent() {
     //   <HeaderButtonComponent />
     // </div>
 
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-yellow">
       <div className="container-fluid">
         <HeaderLogoComponent />
         <div className="collapse navbar-collapse" id="navbarNav">
