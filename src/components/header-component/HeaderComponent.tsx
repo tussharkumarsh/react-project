99f06bcdc89c3a42be2bf31e6eb1f90fd4f98427
Tushar Sharma <tussharkumar.sh@gmail.com>
2025-01-07 15:44:15 +0530

    header page changes

--- src/components/header-component/HeaderComponent.tsx
+++ src/components/header-component/HeaderComponent.tsx
@@ -5,11 +5,6 @@ import HeaderButtonComponent from './HeaderButtonComponent';
 
 function HeaderComponent() {
   return (
-    // <div className='topHeader'>
-    //   <HeaderLogoComponent />
-    //   <HeaderLinksComponent />
-    //   <HeaderButtonComponent />
-    // </div>
 
     <nav className="navbar navbar-expand-lg navbar-light bg-yellow">
       <div className="container-fluid">
@@ -19,7 +14,6 @@ function HeaderComponent() {
 
           <div className="d-flex">
             <HeaderButtonComponent />
-            {/* <a href="#" className="btn btn-outline-primary">Login</a> */}
           </div>
         </div>
       </div>
