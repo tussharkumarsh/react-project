c39e5dc5f97d6614f285a283f0d8c2e47825942d
Tushar Sharma <tussharkumar.sh@gmail.com>
2025-01-16 18:49:01 +0530

    change

--- src/features/cart-page/CartPage.tsx
+++ src/features/cart-page/CartPage.tsx
@@ -16,8 +16,6 @@ function CartPage() {
                         <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" className="wh-100" alt="img" />
                         <h5 className="card-title">Product 1</h5>
                         <p className="card-text">Price: $100</p>
-
-
                         <div className="flex items-center gap-4 p-4 border rounded-lg w-fit">
                             <button onClick={decreaseQuantity}> - </button>
                             <span> {quantity} </span>
