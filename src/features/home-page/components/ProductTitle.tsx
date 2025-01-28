2332204c6e36525188d4a5aa48c7ae00fead54c1
Tushar Sharma <tussharkumar.sh@gmail.com>
2025-01-28 08:11:11 +0530

    change

--- src/features/home-page/components/ProductTitle.tsx
+++ src/features/home-page/components/ProductTitle.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 function ProductTitle(props) {
-    return <h3 className="product-title">
-        <Link to={`/product-detail/${props.id}`}>{props.title}</Link>
+    return <h3 className="product-title"> <Link to={`/product-detail/${props.id}`}>{props.title}</Link>
     </h3>;
 }
 export default ProductTitle;
